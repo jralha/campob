97b50bcbf299466968ad4afc0f552a9b05c6bfd7
jralha <42118093+jralha@users.noreply.github.com>
2019-10-22 11:01:40 -0300

    Completed cluster stats and reordering

--- pycode/cluster_stats.py
+++ pycode/cluster_stats.py
@@ -2,19 +2,52 @@
 import pandas as pd
 import numpy as np
 np.set_printoptions(suppress=True)
+pd.set_option('mode.chained_assignment', None)
 
 #%% Data load
-res = pd.read_csv('csv_correcao\\cluster.csv')
+res = pd.read_csv('campob\\csv_correcao\\cluster.csv')
 
-#%%
+#%% Define datasets, reorder clusters in order of Porosity and SW
 gmm = res[['RHOB','GR','phie','SW','ILD','GMM']]
 kme = res[['RHOB','GR','phie','SW','ILD','KMeans']]
 
-#%%
-kme_describe=kme.groupby('KMeans').describe().T
-gmm_describe=gmm.groupby('GMM').describe().T
+kme_describe=kme.groupby('KMeans').describe()
+gmm_describe=gmm.groupby('GMM').describe()
 
-#%% Reorder clusters in order of Porosity
+kme_phi = kme_describe[[('phie',  'mean'),('SW','mean')]]
+originalOrderK = list(kme_phi.index)
+sortedK = kme_phi.sort_values([('phie',  'mean'),('SW','mean')])
+newOrderK = list(sortedK.index)
+swapK = dict(zip(originalOrderK,newOrderK))
 
+gmm_phi = gmm_describe[[('phie',  'mean'),('SW','mean')]]
+originalOrderG = list(gmm_phi.index)
+sortedG = gmm_phi.sort_values([('phie',  'mean'),('SW','mean')])
+newOrderG = list(sortedG.index)
+swapG = dict(zip(originalOrderG,newOrderG))
 
+gmm['GMM'] = gmm['GMM'].map(swapG)
+kme['KMeans'] = kme['KMeans'].map(swapK)
+
+res['GMM'] = gmm['GMM']
+res['KMeans'] = kme['KMeans']
+
+newNames=[]
+oldNames=[]
+for i,oldName in enumerate(list(swapG.keys())):
+    oldNames.append('cluster '+str(oldName))
+    newNames.append('cluster '+str(list(swapG.values())[i]))
+
+swapNames = dict(zip(oldNames,newNames))
+
+res.rename(swapNames)
+
+res.to_csv('campob\\csv_correcao\\cluster_reorder.csv')
+
+#%% Redo cluster stats
+kme_describe=kme.groupby('KMeans').describe()
+gmm_describe=gmm.groupby('GMM').describe()
+
+kme_describe.to_csv('campob\\csv_correcao\\kme_stats.csv')
+gmm_describe.to_csv('campob\\csv_correcao\\gmm_stats.csv')
 #%%
