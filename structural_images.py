6df9dc358631cd321eac28f604b8d3ec47554867
jralha <42118093+jralha@users.noreply.github.com>
2020-02-10 10:27:20 -0300

    saved struct images

--- structural_images.py
+++ structural_images.py
@@ -23,27 +23,30 @@ sou = res.loc[ res['ycoord'] <= med]
 fn = nor.loc[ (nor['anttrracking'] > -0.5) & (nor['anttrracking'] < 1) ]
 fs = sou.loc[ (sou['anttrracking'] > -0.7) & (sou['anttrracking'] < 1) ]
 
-plt.scatter(fn['xcoord'],fn['ycoord'],s=5,label='Zona Norte - '+str(len(fn))+' pontos com Ant-Tracking > -0.8')
-plt.scatter(fs['xcoord'],fs['ycoord'],s=5,label='Zona Sul - '+str(len(fs))+' pontos com Ant-Tracking > -0.8')
-plt.ylabel('Latitude')
-plt.xlabel('Longitude')
-plt.legend(loc='upper left', bbox_to_anchor=(-0.1, -0.15))
+# plt.scatter(fn['xcoord'],fn['ycoord'],s=5,label='Zona Norte - '+str(len(fn))+' pontos com Ant-Tracking > -0.8')
+# plt.scatter(fs['xcoord'],fs['ycoord'],s=5,label='Zona Sul - '+str(len(fs))+' pontos com Ant-Tracking > -0.8')
+# plt.ylabel('Latitude')
+# plt.xlabel('Longitude')
+# plt.legend(loc='upper left', bbox_to_anchor=(-0.0, 1.15))
+# plt.show()
 
-
-# %%
-plt.scatter(nor['xcoord'],nor['ycoord'],s=5,label='Zona Norte')
-plt.scatter(sou['xcoord'],sou['ycoord'],s=5,label='Zona Sul')
-plt.ylabel('Latitude')
-plt.xlabel('Longitude')
-plt.legend(loc='upper left', bbox_to_anchor=(-0.1, -0.15))
-
+# exit()
 
 # %%
-plt.scatter(nor['xcoord'],nor['anttrracking'],s=5,label='Zona Norte')
-plt.scatter(sou['xcoord'],sou['anttrracking'],s=5,label='Zona Sul')
-plt.xlabel('Longitude')
+# plt.scatter(nor['xcoord'],nor['ycoord'],s=5,label='Zona Norte')
+# plt.scatter(sou['xcoord'],sou['ycoord'],s=5,label='Zona Sul')
+# plt.ylabel('Latitude')
+# plt.xlabel('Longitude')
+# plt.legend(loc='upper left', bbox_to_anchor=(-0.0, 1.15))
+# plt.show()
+# exit()
+
+# %%
+plt.scatter(res['ycoord'],res['anttrracking'],s=0.5,c='black')
+# plt.scatter(fs['ycoord'],fs['anttrracking'],s=0.5,c='black')
+plt.xlabel('Latitude')
 plt.ylabel('Ant-Tracking')
-
+plt.show()
 
 # %%
 
