24415a70923af0e1f38fb7368134bd6adc76bd76
jralha <42118093+jralha@users.noreply.github.com>
2020-05-31 18:47:29 -0300

    map update

--- mapping.py
+++ mapping.py
@@ -17,8 +17,15 @@ res['cluster 1 ou 2'] = 1-res['cluster 0']
 
 #%% Functions
 
-def plot_section(df,const_col,const_val,x,y,color_dim,orientation,plot_type=None,num_colors=None,cmap='magma',sz=15,savefile=None):
+def plot_section(df,const_col,const_val,x,y,color_dim,orientation,plot_type=None,num_colors=3,cmap='plasma',savefile=None):
     plot_data = df.loc[df[const_col] == const_val]
+
+    nbins=np.ceil(np.sqrt(len(plot_data)))
+    zi, yi, xi = np.histogram2d(plot_data[y], plot_data[x], bins=(nbins,nbins), weights=plot_data[color_dim], normed=False)
+    counts, _, _ = np.histogram2d(plot_data[y], plot_data[x], bins=(nbins,nbins))
+
+    zi = zi / counts
+    zi = np.ma.masked_invalid(zi)
 
     if orientation == 'depth':
         plt.figure(figsize=[8,8])
@@ -27,10 +34,13 @@ def plot_section(df,const_col,const_val,x,y,color_dim,orientation,plot_type=None
 
     if plot_type == 'discrete': 
         c_map = plt.get_cmap(cmap, num_colors)
-        plt.scatter(plot_data[x],plot_data[y],c=plot_data[color_dim],cmap=c_map,s=sz)
+        # plt.pcolormesh(xi[:-1], yi[:-1], zi,snap=True,cmap=c_map,rasterized=True)
+        plt.scatter(plot_data[x],plot_data[y],c=plot_data[color_dim],cmap=c_map)
         plt.colorbar(ticks=range(num_colors),label=color_dim)
     elif plot_type == 'continuous':
-        plt.scatter(plot_data[x],plot_data[y],c=plot_data[color_dim],cmap=cmap,s=sz)
+        # plt.pcolormesh(xi, yi, zi,cmap=cmap)
+        plt.scatter(plot_data[x],plot_data[y],c=plot_data[color_dim],cmap=cmap,s=3)
+
         plt.colorbar(label=color_dim)
 
     
@@ -40,31 +50,31 @@ def plot_section(df,const_col,const_val,x,y,color_dim,orientation,plot_type=None
     plt.show()
 
 
-#%%
-plot_section(res,'cellk',med_k,'xcoord','ycoord','cluster 0','depth',plot_type='continuous')
+
+# plot_section(res,'cellk',med_k,'xcoord','ycoord','cluster 0','depth',plot_type='continuous')
 # plot_section(res,'cellk',med_k,'xcoord','ycoord','cluster 1','depth',plot_type='continuous')
 # plot_section(res,'cellk',med_k,'xcoord','ycoord','cluster 2','depth',plot_type='continuous')
 # plot_section(res,'cellk',med_k,'xcoord','ycoord','cluster 1 ou 2','depth',plot_type='continuous')
-# plot_section(res,'cellk',med_k,'xcoord','ycoord','GMM','depth',plot_type='discrete',num_colors=3)
-# plot_section(res,'cellk',med_k,'xcoord','ycoord','KMeans','depth',plot_type='discrete',num_colors=3)
+# plot_section(res,'cellk',med_k,'xcoord','ycoord','GMM','depth',plot_type='discrete')
+# plot_section(res,'cellk',med_k,'xcoord','ycoord','KMeans','depth',plot_type='discrete')
 # plot_section(res,'cellk',med_k,'xcoord','ycoord','sw','depth',plot_type='continuous')
 # plot_section(res,'cellk',med_k,'xcoord','ycoord','phie','depth',plot_type='continuous')
 # plot_section(res,'cellk',med_k,'xcoord','ycoord','rhob','depth',plot_type='continuous')
 # plot_section(res,'cellk',med_k,'xcoord','ycoord','gr','depth',plot_type='continuous')
 # plot_section(res,'cellk',med_k,'xcoord','ycoord','dt','depth',plot_type='continuous')
 
-#%%
-plot_section(res,'cellj',med_j,'xcoord','zcoord','cluster 0','vertical',plot_type='continuous')
-plot_section(res,'cellj',med_j,'xcoord','zcoord','cluster 1','vertical',plot_type='continuous')
-plot_section(res,'cellj',med_j,'xcoord','zcoord','cluster 2','vertical',plot_type='continuous')
-plot_section(res,'cellj',med_j,'xcoord','zcoord','cluster 1 ou 2','vertical',plot_type='continuous')
-plot_section(res,'cellj',med_j,'xcoord','zcoord','GMM','vertical',plot_type='discrete',num_colors=3)
-plot_section(res,'cellj',med_j,'xcoord','zcoord','KMeans','vertical',plot_type='discrete',num_colors=3)
-plot_section(res,'cellj',med_j,'xcoord','zcoord','sw','vertical',plot_type='continuous')
-plot_section(res,'cellj',med_j,'xcoord','zcoord','phie','vertical',plot_type='continuous')
-plot_section(res,'cellj',med_j,'xcoord','zcoord','rhob','vertical',plot_type='continuous')
-plot_section(res,'cellj',med_j,'xcoord','zcoord','gr','vertical',plot_type='continuous')
-plot_section(res,'cellj',med_j,'xcoord','zcoord','dt','vertical',plot_type='continuous')
+
+# plot_section(res,'cellj',med_j,'xcoord','zcoord','cluster 0','vertical',plot_type='continuous')
+# plot_section(res,'cellj',med_j,'xcoord','zcoord','cluster 1','vertical',plot_type='continuous')
+# plot_section(res,'cellj',med_j,'xcoord','zcoord','cluster 2','vertical',plot_type='continuous')
+# plot_section(res,'cellj',med_j,'xcoord','zcoord','cluster 1 ou 2','vertical',plot_type='continuous')
+plot_section(res,'cellj',med_j,'xcoord','zcoord','GMM','vertical',plot_type='discrete')
+# plot_section(res,'cellj',med_j,'xcoord','zcoord','KMeans','vertical',plot_type='discrete')
+# plot_section(res,'cellj',med_j,'xcoord','zcoord','sw','vertical',plot_type='continuous')
+# plot_section(res,'cellj',med_j,'xcoord','zcoord','phie','vertical',plot_type='continuous')
+# plot_section(res,'cellj',med_j,'xcoord','zcoord','rhob','vertical',plot_type='continuous')
+# plot_section(res,'cellj',med_j,'xcoord','zcoord','gr','vertical',plot_type='continuous')
+# plot_section(res,'cellj',med_j,'xcoord','zcoord','dt','vertical',plot_type='continuous')
 
 
 # %%
